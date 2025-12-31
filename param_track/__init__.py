e0dfaab02b42eb256b7d2bc0dfdd248426c7096b
DAVID DEBOER <ddeboer@berkeley.edu>
2025-12-31 13:41:26 -0800

    clarified MIT license

--- param_track/__init__.py
+++ param_track/__init__.py
@@ -1,6 +1,6 @@
 # -*- mode: python; coding: utf-8 -*-
 # Copyright 2025 David R DeBoer
-# Licensed under the BSD license. See LICENSE.txt file in the project root for details.
+# Licensed under the MIT license. See LICENSE file in the project root for details.
 
 
 """A simple parameter tracking class"""
