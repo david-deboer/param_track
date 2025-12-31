cb89593d188640971d305771337ab4f3a44dd116
DAVID DEBOER <ddeboer@berkeley.edu>
2025-12-31 13:24:52 -0800

    add copyright and rename license file

--- param_track/param_track_error.py
+++ param_track/param_track_error.py
@@ -1,3 +1,7 @@
+# -*- mode: python; coding: utf-8 -*-
+# Copyright 2025 David R DeBoer
+# Licensed under the BSD license. See LICENSE.txt file in the project root for details.
+
 class ParameterTrackError(Exception):
     """Parameter track exception handling."""
     def __init__(self, message):
